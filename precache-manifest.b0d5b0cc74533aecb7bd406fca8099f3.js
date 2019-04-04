92a39c62796444b427b6fd3524b996838ce7833a
ievgen.s <ievgen.s@maxitechs.com>
2019-04-04 22:11:46 +0300

    Updates

---
+++ precache-manifest.b0d5b0cc74533aecb7bd406fca8099f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29dd4ad8f1ecacf31a1f",
+    "url": "/youtubeVideo/static/js/runtime~main.656fa706.js"
+  },
+  {
+    "revision": "d892c2b3f5ebd3b9c315",
+    "url": "/youtubeVideo/static/js/main.93933539.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/youtubeVideo/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "d892c2b3f5ebd3b9c315",
+    "url": "/youtubeVideo/static/css/main.68862216.chunk.css"
+  },
+  {
+    "revision": "7139321d14bfe5eee7b5add0abb6d06f",
+    "url": "/youtubeVideo/index.html"
+  }
+];
